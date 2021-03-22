0e50dd1f7fc8b7fc70eeeb5f83d75adb41b36c8d
Ambitious156288 <ambitious156288@gmail.com>
2021-03-22 19:59:14 +0000

    mongodb connect throught app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+app.use("/posts", postRoutes);
+
+// app.use(bodyParser.json({ limit: "30mb", exntended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", exntended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin@mern-project.6eyo7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
