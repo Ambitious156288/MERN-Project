d6ab45bb75e585fe91c9917436177d02f8af1300
Ambitious156288 <ambitious156288@gmail.com>
2021-03-23 00:34:24 +0000

    getPost, createPost functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,25 @@
-export const getPosts = (req, res) => {
-  res.send("works");
+import PostMessage from "../models/PostMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+
+    res.status(200).json(postMessage);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
 };
