c469d90898f55a1d42091f4d70985b02751ad30e
Ambitious156288 <ambitious156288@gmail.com>
2021-03-25 18:06:35 +0000

    network error fix

--- server/index.js
+++ server/index.js
@@ -7,15 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
-// app.use(bodyParser.json({ limit: "30mb", exntended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", exntended: true }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://admin:admin@mern-project.6eyo7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
