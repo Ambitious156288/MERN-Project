9381a7dbc25c148684f8a186ef92340954eb86ba
Ambitious156288 <ambitious156288@gmail.com>
2021-04-04 21:03:50 +0100

    environmental variables

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
-const app = express();
+import dotenv from "dotenv";
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -17,12 +17,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://admin:admin@mern-project.6eyo7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`server is running on ${PORT}`))
   )
