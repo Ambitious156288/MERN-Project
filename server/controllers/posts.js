9a2cd25d7fe97aac791fbafc7e5d2e8074cb2782
Ambitious156288 <ambitious156288@gmail.com>
2021-04-08 06:12:46 +0100

    heroku and github pages

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import PostMessage from "../models/PostMessage.js";
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
   try {
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
-  res.send("/");
+  res.send("memories API");
 });
 
 const PORT = process.env.PORT || 5000;
