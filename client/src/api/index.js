daa584c838ab9c2b9fb3fad064a3f0f8cd174cff
Ambitious156288 <ambitious156288@gmail.com>
2021-04-28 13:29:53 +0100

    heroku backend url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import routes from 'constants/routes';
 import { user } from 'constants/userConstant';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memorable-events.herokuapp.com/' });
 
 API.interceptors.request.use(req => {
   if (localStorage.getItem('profile')) {
