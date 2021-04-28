53b6e7cbe64229a2ec711b6ec23326187e8dfa5f
Ambitious156288 <ambitious156288@gmail.com>
2021-04-28 13:30:38 +0100

    Updates

---
+++ precache-manifest.95835f87e49c03e444d804ee66f90227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550fc76cd6e10457300876b5c3e9d798",
+    "url": "/MERN-Project/index.html"
+  },
+  {
+    "revision": "19cd27df6a8c63ba15a5",
+    "url": "/MERN-Project/static/css/main.fed748a9.chunk.css"
+  },
+  {
+    "revision": "080ae39b3777fce60aa1",
+    "url": "/MERN-Project/static/js/2.6056c457.chunk.js"
+  },
+  {
+    "revision": "702664421703a10b78a9c0d5af38339c",
+    "url": "/MERN-Project/static/js/2.6056c457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cd27df6a8c63ba15a5",
+    "url": "/MERN-Project/static/js/main.8a90430d.chunk.js"
+  },
+  {
+    "revision": "d1b1d28f1af263b19421",
+    "url": "/MERN-Project/static/js/runtime-main.07e5b636.js"
+  },
+  {
+    "revision": "c183954d46fae01f6beb3e3754191413",
+    "url": "/MERN-Project/static/media/defaultPostImage.c183954d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.016053994b1ef4882eb97a746cff4f00.js"
+  "/MERN-Project/precache-manifest.95835f87e49c03e444d804ee66f90227.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MERN-Project/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
